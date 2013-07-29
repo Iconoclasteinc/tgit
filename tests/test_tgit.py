debd20f13afc3c5be14464ea485b26099e4bfd3d
testinfected <vtence@gmail.com>
2013-07-29 14:35:23 -0400

    An attempt to run the test thread in the background, since Qt main event loop will only run on the main thread

--- tests/test_tgit.py
+++ tests/test_tgit.py
@@ -1,23 +1,25 @@
 import unittest
 
 from PyQt4.QtGui import QApplication
-from PyQt4.QtTest import QTest
-from PyQt4.QtCore import Qt
 
 from tgit.tgit import TgIT
+
+import thread
 
 class TgitTest(unittest.TestCase):
 
     def setUp(self):
-        '''Create the GUI'''
-        self.app = QApplication([])
-        self.driver = TgIT()
-        self.driver.show()
+        pass
+
+    def tearDown(self):
+        QApplication.quit()
 
     def test_doing_nothing(self):
-        self.app.exec_()
         self.assertTrue(True)
 
-
 if __name__ == "__main__":
-    unittest.main()
+    app = QApplication([])
+    driver = TgIT()
+    driver.show()
+    t = thread.start_new_thread(unittest.main, ())
+    app.exec_()
