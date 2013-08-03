9647d4bf8f76420ccf3be3ab7c2b55307e82ceae
testinfected <vtence@gmail.com>
2013-08-03 12:04:41 +0200

    To drive UI in tests, find top level window in application component tree

--- tests/test_tgit.py
+++ tests/test_tgit.py
@@ -10,17 +10,34 @@ class TGiTTest(unittest.TestCase):
 
     def setUp(self):
         self.app = QApplication([])
-        self.driver = TGiT()
+        self.tgit = TGiT()
+        self.driver = self._single_widget(self._root_widgets())
         self.driver.show()
         QTest.qWaitForWindowShown(self.driver)
 
     def tearDown(self):
         self.driver.close()
+        del self.tgit
         del self.app
 
     def test_clicking_on_button_to_demonstrate_simulating_events(self):
         button = self.driver.findChild(QPushButton)
         button.click()
+
+    def _root_widgets(self):
+        root_widgets = set()
+        for top_level_widget in self.app.topLevelWidgets():
+            root_widgets.add(self._root_parent(top_level_widget))
+
+        return root_widgets
+
+    def _root_parent(self, widget):
+        return widget if not widget.parent() else self._root_parent(widget.parent())
+
+    def _single_widget(self, widgets):
+        if len(widgets) != 1: self.fail("Expected exactly 1 top level window, but got " + str(len(widgets)))
+        return widgets.pop()
+
 
 if __name__ == "__main__":
     unittest.main()
