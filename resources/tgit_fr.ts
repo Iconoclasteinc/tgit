7da57ffef593871a4223517e66a1fbf2dc48fe2e
testinfected <vtence@gmail.com>
2013-09-13 13:18:28 -0400

    Translate user interface labels in french

--- resources/tgit_fr.ts
+++ resources/tgit_fr.ts
@@ -1,27 +1,61 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="fr_CA" sourcelanguage="en_US">
+<!DOCTYPE TS><TS version="2.0" language="fr_CA" sourcelanguage="en_US">
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="main_window.py" line="98"/>
+        <location filename="main_window.py" line="135"/>
         <source>TGiT</source>
-        <translation>TGiT</translation>
+        <translation type="unfinished">TGiT</translation>
     </message>
     <message>
-        <location filename="main_window.py" line="99"/>
+        <location filename="main_window.py" line="136"/>
         <source>Add File...</source>
-        <translation>Ajouter un fichier...</translation>
+        <translation type="unfinished">Ajouter un fichier...</translation>
     </message>
     <message>
-        <location filename="main_window.py" line="100"/>
+        <location filename="main_window.py" line="137"/>
         <source>Quit</source>
-        <translation>Quitter</translation>
+        <translation type="unfinished">Quitter</translation>
     </message>
     <message>
-        <location filename="main_window.py" line="101"/>
+        <location filename="main_window.py" line="138"/>
         <source>Hit me to quit</source>
-        <translation>Cliquer pour quitter</translation>
+        <translation type="unfinished">Cliquer pour quitter</translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="139"/>
+        <source>Album Title: </source>
+        <translation type="unfinished">Titre de l'album:</translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="140"/>
+        <source>Album Artist: </source>
+        <translation type="unfinished">Artiste principal:</translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="141"/>
+        <source>Track Title: </source>
+        <translation type="unfinished">Titre de la piste:</translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="142"/>
+        <source>Bitrate: </source>
+        <translation type="unfinished">Débit:</translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="143"/>
+        <source>Duration: </source>
+        <translation type="unfinished">Durée:</translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="144"/>
+        <source>Save</source>
+        <translation type="unfinished">Enregistrer</translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="145"/>
+        <source>MP3 files</source>
+        <translation type="unfinished">Fichiers MP3</translation>
     </message>
 </context>
 </TS>
