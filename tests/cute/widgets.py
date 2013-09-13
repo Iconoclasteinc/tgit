132896d1d32d7bb53a706cfd6754bc62a09fb16b
testinfected <vtence@gmail.com>
2013-09-12 20:53:04 -0400

    Even if it feels unnatural that widgets perform gestures, it simplifies code, makes it easier to understand and less error prone. It also eliminates temporal dependencies between gestures.

--- tests/cute/widgets.py
+++ tests/cute/widgets.py
@@ -17,9 +17,10 @@ def main_window(*matchers):
 
 
 class WidgetDriver(object):
-    def __init__(self, selector, prober):
+    def __init__(self, selector, prober, gesture_performer):
         self._selector = selector
         self._prober = prober
+        self._gesture_performer = gesture_performer
 
     @property
     def selector(self):
@@ -29,10 +30,15 @@ class WidgetDriver(object):
     def prober(self):
         return self._prober
 
+    @property
+    def gesture_performer(self):
+        return self._gesture_performer
+
     @classmethod
     def find(cls, parent, widget_type, *matchers):
         return cls(SingleWidgetFinder(
-            RecursiveWidgetFinder(widget_type, all_of(*matchers), parent.selector)), parent.prober)
+            RecursiveWidgetFinder(widget_type, all_of(*matchers), parent.selector)),
+            parent.prober, parent.gesture_performer)
 
     def is_showing_on_screen(self):
         self.verify(match.showing_on_screen())
@@ -53,7 +59,10 @@ class WidgetDriver(object):
 
     def left_click_on_widget(self):
         self.is_showing_on_screen()
-        return gestures.click_on(self.widget_center())
+        self.perform(gestures.click_on(self.widget_center()))
+
+    def perform(self, *gestures):
+        self.gesture_performer.perform(*gestures)
 
     def _check(self, probe):
         self.prober.check(probe)
@@ -80,24 +89,26 @@ class LineEditDriver(WidgetDriver):
         self.has(properties.input_text(), equal_to(text))
 
     def replace_all_text(self, text):
-        return gestures.sequence(self.focus_with_mouse(), self.clear_all_text(), self.type(text))
+        self.focus_with_mouse()
+        self.clear_all_text()
+        self.type(text)
 
     def focus_with_mouse(self):
-        return self.left_click_on_widget()
+        self.left_click_on_widget()
 
     def clear_all_text(self):
-        return gestures.sequence(self.select_all_text(),
-                                 gestures.pause(50), self.delete_selected_text())
+        self.select_all_text()
+        self.delete_selected_text()
 
     def select_all_text(self):
         # todo probably better to try key sequence Select-All (CTRL+A for Qt)
-        return gestures.mouse_double_click()
+        self.perform(gestures.mouse_double_click())
 
     def delete_selected_text(self):
-        return gestures.type_key(Qt.Key_Backspace)
+        self.perform(gestures.type_key(Qt.Key_Backspace))
 
     def type(self, text):
-        return gestures.type_text(text)
+        self.perform(gestures.type_text(text))
 
 
 class FileDialogDriver(WidgetDriver):
@@ -110,13 +121,11 @@ class FileDialogDriver(WidgetDriver):
         dialog.setFilter(dialog.filter() | QDir.Hidden)
 
     def navigate_to_dir(self, path):
-        def change_to(name):
-            if name == '..':
-                return lambda robot: self.up_one_folder()(robot)
+        for folder_name in self._navigation_path_to(path):
+            if folder_name == '..':
+                self.up_one_folder()
             else:
-                return lambda robot: self.into_folder(name)(robot)
-
-        return gestures.sequence(*[change_to(d) for d in self._navigation_path_to(path)])
+                self.into_folder(folder_name)
 
     def _navigation_path_to(self, path):
         return self._current_dir().relativeFilePath(path).split("/")
@@ -131,25 +140,27 @@ class FileDialogDriver(WidgetDriver):
         return current_dir.name
 
     def into_folder(self, name):
-        return gestures.sequence(self.select_file(name),
-                                 gestures.pause(self.NAVIGATION_DELAY),
-                                 gestures.mouse_double_click(),
-                                 gestures.pause(self.NAVIGATION_DELAY))
+        self.select_file(name)
+        self._double_click_on_folder()
+
+    def _double_click_on_folder(self):
+        self.perform(gestures.mouse_double_click(),
+                     gestures.pause(self.NAVIGATION_DELAY))
 
     def select_file(self, name):
-        return self._list_view().select_item(match.with_list_item_text(name))
+        self._list_view().select_item(match.with_list_item_text(name))
 
     def up_one_folder(self):
-        return self._tool_button_named('toParentButton').click()
+        self._tool_button_named('toParentButton').click()
 
     def _tool_button_named(self, name):
         return AbstractButtonDriver.find(self, QToolButton, match.named(name))
 
     def enter_manually(self, filename):
-        return self._filename_edit().replace_all_text(filename)
+        self._filename_edit().replace_all_text(filename)
 
     def accept(self):
-        return self._accept_button().click()
+        self._accept_button().click()
 
     def _list_view(self):
         return ListViewDriver.find(self, QListView, match.named('listView'))
@@ -165,11 +176,11 @@ class FileDialogDriver(WidgetDriver):
 
 class ListViewDriver(WidgetDriver):
     def select_item(self, matcher):
-        return self._select_item(self._index_of_first_item_matching(matcher))
+        self._select_item(self._index_of_first_item_matching(matcher))
 
     def _select_item(self, index):
         self._scroll_item_to_visible(index)
-        return gestures.click_on(self._center_of_item(index))
+        self.perform(gestures.click_on(self._center_of_item(index)))
 
     def _scroll_item_to_visible(self, index):
         self.manipulate("scroll item to visible", lambda list_view: list_view.scrollTo(index))
--- tests/endtoend/tgit_driver.py
+++ tests/endtoend/tgit_driver.py
@@ -17,8 +17,8 @@ class TGiTDriver(MainWindowDriver):
     def __init__(self, timeout_in_ms):
         super(TGiTDriver, self).__init__(
             main_window(named(main.MAIN_WINDOW_NAME), showing_on_screen()),
-            EventProcessingProber(timeout_in_ms=timeout_in_ms))
-        self._robot = Robot()
+            EventProcessingProber(timeout_in_ms=timeout_in_ms),
+            Robot())
 
     def add_file(self, path):
         self._open_add_file_dialog()
@@ -36,28 +36,23 @@ class TGiTDriver(MainWindowDriver):
     def _open_add_file_dialog(self):
         add_file_button = AbstractButtonDriver.find(self, QPushButton,
                                                     named(main.ADD_FILE_BUTTON_NAME))
-        self._perform_gestures(add_file_button.click())
+        add_file_button.click()
 
     def _select_file(self, path):
         dialog = FileDialogDriver.find(self, QFileDialog)
         dialog.show_hidden_files()
-        self._perform_gestures(dialog.navigate_to_dir(os.path.dirname(path)))
-        # Execute the navigation gesture first
-        self._perform_gestures(dialog.select_file(os.path.basename(path)))
+        dialog.navigate_to_dir(os.path.dirname(path))
+        dialog.select_file(os.path.basename(path))
 
     def _accept_file(self):
         dialog = FileDialogDriver.find(self, QFileDialog)
-        self._perform_gestures(dialog.accept())
+        dialog.accept()
 
     def edit_metadata(self, album):
         album_title_input = LineEditDriver.find(self, QLineEdit, named(main.ALBUM_TITLE_INPUT_NAME))
-        self._perform_gestures(album_title_input.focus_with_mouse(),
-                               album_title_input.replace_all_text(album))
+        album_title_input.focus_with_mouse()
+        album_title_input.replace_all_text(album)
 
     def save_audio_file(self):
         save_button = AbstractButtonDriver.find(self, QPushButton, named(main.SAVE_BUTTON_NAME))
-        self._perform_gestures(save_button.click())
-
-    def _perform_gestures(self, *gestures):
-        self._robot.perform(*gestures)
-
+        save_button.click()
