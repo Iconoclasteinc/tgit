7eefe2ab687a55069adee1d29fc12f0bd32f52aa
testinfected <vtence@gmail.com>
2013-09-29 22:59:26 -0400

    Update french translations for release

--- locales/tgit_fr.ts
+++ locales/tgit_fr.ts
@@ -1,96 +1,113 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="fr_CA" sourcelanguage="en_US">
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_CA" sourcelanguage="en_US">
 <context>
-    <name>MainWindow</name>
+    <name>AlbumPanel</name>
     <message>
-        <location filename="main_window.py" line="268"/>
-        <source>TGiT</source>
-        <translation type="unfinished">TGiT</translation>
+        <location filename="album_panel.py" line="130"/>
+        <source>Select Picture...</source>
+        <translation type="unfinished">Choisir une image...</translation>
     </message>
     <message>
-        <location filename="main_window.py" line="269"/>
-        <source>Add File...</source>
-        <translation type="unfinished">Importer un fichier...</translation>
-    </message>
-    <message>
-        <location filename="main_window.py" line="270"/>
-        <source>Quit</source>
-        <translation type="unfinished">Quitter</translation>
-    </message>
-    <message>
-        <location filename="main_window.py" line="271"/>
-        <source>Hit me to quit</source>
-        <translation type="unfinished">Cliquer pour quitter</translation>
-    </message>
-    <message>
-        <location filename="main_window.py" line="276"/>
-        <source>Track Title: </source>
-        <translation type="unfinished">Titre de la piste: </translation>
-    </message>
-    <message>
-        <location filename="main_window.py" line="280"/>
-        <source>Bitrate: </source>
-        <translation type="unfinished">Débit: </translation>
-    </message>
-    <message>
-        <location filename="main_window.py" line="281"/>
-        <source>Duration: </source>
-        <translation type="unfinished">Durée:</translation>
-    </message>
-    <message>
-        <location filename="main_window.py" line="282"/>
-        <source>Save</source>
-        <translation type="unfinished">Enregistrer</translation>
-    </message>
-    <message>
-        <location filename="main_window.py" line="283"/>
-        <source>MP3 files</source>
-        <translation type="unfinished">Fichiers MP3</translation>
-    </message>
-    <message>
-        <location filename="main_window.py" line="272"/>
-        <source>Release Name: </source>
-        <translation type="unfinished">Titre de l&apos;album: </translation>
-    </message>
-    <message>
-        <location filename="main_window.py" line="273"/>
-        <source>Lead Performer: </source>
-        <translation type="unfinished">Artiste principal: </translation>
-    </message>
-    <message>
-        <location filename="main_window.py" line="274"/>
-        <source>Original Release Date: </source>
-        <translation type="unfinished">Date de mise en marché: </translation>
-    </message>
-    <message>
-        <location filename="main_window.py" line="275"/>
-        <source>UPC/EAN: </source>
-        <translation type="unfinished">UPC/EAN: </translation>
-    </message>
-    <message>
-        <location filename="main_window.py" line="277"/>
-        <source>Version Information: </source>
-        <translation type="unfinished">Infos sur la version: </translation>
-    </message>
-    <message>
-        <location filename="main_window.py" line="278"/>
-        <source>Featured Guest: </source>
-        <translation type="unfinished">Invité spécial: </translation>
-    </message>
-    <message>
-        <location filename="main_window.py" line="279"/>
-        <source>ISRC: </source>
-        <translation type="unfinished">ISRC: </translation>
-    </message>
-    <message>
-        <location filename="main_window.py" line="285"/>
+        <location filename="album_panel.py" line="131"/>
         <source>Image files</source>
         <translation type="unfinished">Fichiers images</translation>
     </message>
     <message>
-        <location filename="main_window.py" line="284"/>
-        <source>Select Picture...</source>
-        <translation type="unfinished">Choisir une image...</translation>
+        <location filename="album_panel.py" line="132"/>
+        <source>Release Name: </source>
+        <translation type="unfinished">Titre de l&apos;album: </translation>
+    </message>
+    <message>
+        <location filename="album_panel.py" line="133"/>
+        <source>Lead Performer: </source>
+        <translation type="unfinished">Artiste principal: </translation>
+    </message>
+    <message>
+        <location filename="album_panel.py" line="134"/>
+        <source>Release Date: </source>
+        <translation type="unfinished">Date de sortie:</translation>
+    </message>
+    <message>
+        <location filename="album_panel.py" line="135"/>
+        <source>UPC/EAN: </source>
+        <translation type="unfinished">UPC/EAN: </translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="main_window.py" line="159"/>
+        <source>TGiT</source>
+        <translation type="unfinished">TGiT</translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="160"/>
+        <source>Add File...</source>
+        <translation type="unfinished">Importer un fichier...</translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="161"/>
+        <source>Quit</source>
+        <translation type="unfinished">Quitter</translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="162"/>
+        <source>Hit me to quit</source>
+        <translation type="unfinished">Cliquer pour quitter</translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="163"/>
+        <source>Save</source>
+        <translation type="unfinished">Enregistrer</translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="166"/>
+        <source>MP3 files</source>
+        <translation type="unfinished">Fichiers MP3</translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="164"/>
+        <source>Previous</source>
+        <translation type="unfinished">Précédent</translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="165"/>
+        <source>Next</source>
+        <translation type="unfinished">Suivant</translation>
+    </message>
+</context>
+<context>
+    <name>TrackPanel</name>
+    <message>
+        <location filename="track_panel.py" line="106"/>
+        <source>Track Title: </source>
+        <translation type="unfinished">Titre de la piste: </translation>
+    </message>
+    <message>
+        <location filename="track_panel.py" line="107"/>
+        <source>Version Information: </source>
+        <translation type="unfinished">Infos sur la version: </translation>
+    </message>
+    <message>
+        <location filename="track_panel.py" line="108"/>
+        <source>Featured Guest: </source>
+        <translation type="unfinished">Invité spécial: </translation>
+    </message>
+    <message>
+        <location filename="track_panel.py" line="109"/>
+        <source>ISRC: </source>
+        <translation type="unfinished">ISRC: </translation>
+    </message>
+    <message>
+        <location filename="track_panel.py" line="110"/>
+        <source>Bitrate: </source>
+        <translation type="unfinished">Débit: </translation>
+    </message>
+    <message>
+        <location filename="track_panel.py" line="111"/>
+        <source>Duration: </source>
+        <translation type="unfinished">Durée:</translation>
     </message>
 </context>
 </TS>
