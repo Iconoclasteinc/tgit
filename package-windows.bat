dab71a04c7bd17bdefea85ef84fc0d1580b856e0
Jonathan Falardeau <jfalardeau@pyxis-tech.com>
2015-04-01 16:40:23 -0400

    Automatically sign the setup file with self-signed certificate.

--- package-windows.bat
+++ package-windows.bat
@@ -1,9 +1,11 @@
 @echo off
 SET CC="C:\Program Files (x86)\Inno Setup 5\ISCC.exe"
+SET SIGN="C:\Program Files (x86)\Windows Kits\8.1\bin\x86\signtool.exe"
 SET VIRTUALENV="%HOMEPATH%\.virtualenvs\TGiT34"
 SET ACTIVATEVIRTUALENV="%VIRTUALENV%\Scripts\activate.bat"
 SET DEACTIVATEVIRTUALENV="%VIRTUALENV%\Scripts\deactivate.bat"
 SET SOURCEDIR="%HOMEPATH%\Documents\Code\tgit"
+SET CERTDIR="..\.."
 
 CD %SOURCEDIR%
 
@@ -12,4 +14,5 @@ lrelease.exe resources\tgit_fr.ts -qm resources\tgit_fr.qm
 pyrcc5.exe -o tgit\ui\resources.py resources.qrc
 python.exe setup.py build
 %CC% tgit_installer.iss
+%SIGN% sign /v /f %CERTDIR%\Iconoclaste.pfx build\*.exe
 CALL %DEACTIVATEVIRTUALENV%
