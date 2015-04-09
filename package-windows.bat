09823b2954c4cef8a627d1e3c1db9644f9abbdb7
Jonathan Falardeau <jfalardeau@pyxis-tech.com>
2015-04-09 10:44:42 -0400

    packaging batch file works outside of virtualenv.

--- package-windows.bat
+++ package-windows.bat
@@ -1,18 +1,19 @@
 @echo off
 SET CC="C:\Program Files (x86)\Inno Setup 5\ISCC.exe"
 SET SIGN="C:\Program Files (x86)\Windows Kits\8.1\bin\x86\signtool.exe"
-SET VIRTUALENV="%HOMEPATH%\.virtualenvs\TGiT34"
-SET ACTIVATEVIRTUALENV="%VIRTUALENV%\Scripts\activate.bat"
-SET DEACTIVATEVIRTUALENV="%VIRTUALENV%\Scripts\deactivate.bat"
+SET PYTHONDIR="C:\Python34"
+SET PYTHONSITEPACKAGESDIR="%PYTHONDIR%\Lib\site-packages"
+SET PYQTDIR="%PYTHONSITEPACKAGESDIR%\PyQt5"
+SET PYTHON="%PYTHONDIR%\python.exe"
+SET PYRCC="%PYQTDIR%\pyrcc5.exe"
+SET LRELEASE="%PYQTDIR%\lrelease.exe"
 SET SOURCEDIR="%HOMEPATH%\Documents\Code\tgit"
 SET CERTDIR="..\.."
 
 CD %SOURCEDIR%
 
-CALL %ACTIVATEVIRTUALENV%
-lrelease.exe resources\tgit_fr.ts -qm resources\tgit_fr.qm
-pyrcc5.exe -o tgit\ui\resources.py resources.qrc
-python.exe setup.py build
+%LRELEASE% resources\tgit_fr.ts -qm resources\tgit_fr.qm
+%PYRCC% -o tgit\ui\resources.py resources.qrc
+%PYTHON% setup.py build
 %CC% tgit_installer.iss
 REM %SIGN% sign /v /f %CERTDIR%\Iconoclaste.pfx build\*.exe
-CALL %DEACTIVATEVIRTUALENV%
