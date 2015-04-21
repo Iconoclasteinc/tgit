4ef4fd5a1170a400b1deb4e5224d40b33a746e2b
testinfected <vtence@gmail.com>
2015-04-20 21:08:41 -0400

    Make qt app fixture a parameter of test method to avoid erratic failures caused by fixtures not being finalized in the right order

--- test/integration/ui/test_track_selection_dialog.py
+++ test/integration/ui/test_track_selection_dialog.py
@@ -6,8 +6,8 @@ from hamcrest import contains
 from PyQt5.QtWidgets import QMainWindow, QFileDialog
 import pytest
 
-from cute.prober import EventProcessingProber
-from cute.robot import Robot
+from test.cute.prober import EventProcessingProber
+from test.cute.robot import Robot
 from test.cute.matchers import named
 from test.cute.widgets import window
 from test.drivers.track_selection_dialog_driver import TrackSelectionDialogDriver
@@ -18,7 +18,7 @@ from tgit.ui.track_selection_dialog import TrackSelectionDialog
 
 
 @pytest.fixture()
-def dialog(app):
+def dialog():
     main_window = QMainWindow()
     show_widget(main_window)
     return TrackSelectionDialog(main_window, native=False, transient=False)
@@ -32,7 +32,7 @@ def driver():
     driver.close()
 
 
-def test_signals_when_audio_files_selected(driver, dialog):
+def test_signals_when_audio_files_selected(app, driver, dialog):
     audio_files = [resources.path('audio', 'Rolling in the Deep.mp3'),
                    resources.path('audio', 'Set Fire to the Rain.mp3'),
                    resources.path('audio', 'Someone Like You.mp3')]
@@ -42,11 +42,11 @@ def test_signals_when_audio_files_selected(driver, dialog):
     dialog.tracks_selected.connect(track_selection_signal.received)
 
     dialog.display()
-    driver.select_tracks(*audio_files)
+    driver.select_tracks('mp3', *audio_files)
     driver.check(track_selection_signal)
 
 
-def test_alternatively_selects_directories_instead_of_files(driver, dialog):
+def test_alternatively_selects_directories_instead_of_files(app, driver, dialog):
     audio_folder = resources.path('audio')
     track_selection_signal = ValueMatcherProbe('track(s) selection', contains(audio_folder))
     dialog.tracks_selected.connect(track_selection_signal.received)
@@ -56,19 +56,19 @@ def test_alternatively_selects_directories_instead_of_files(driver, dialog):
     driver.check(track_selection_signal)
 
 
-def test_allows_selection_of_flac_files(driver, dialog):
+def test_allows_selection_of_flac_files(app, driver, dialog):
     flac_files = [resources.path('audio', 'Zumbar.flac')]
     track_selection_signal = ValueMatcherProbe('track(s) selection', flac_files)
 
     dialog.tracks_selected.connect(track_selection_signal.received)
 
     dialog.display()
-    driver.select_tracks(*flac_files, of_type='flac')
+    driver.select_tracks('flac', *flac_files)
     driver.check(track_selection_signal)
 
 
 @pytest.mark.skipif(sys.platform.startswith("win"), reason="not supported on Windows")
-def test_rejects_non_audio_files(driver, dialog):
+def test_rejects_non_audio_files(app, driver, dialog):
     unsupported_file = resources.path('front-cover.jpg')
 
     dialog.display(folders=False)
