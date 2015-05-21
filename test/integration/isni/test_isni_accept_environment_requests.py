c618a7e7b7f8e46ce9208b849d23a389c7454b3c
Jonathan Falardeau <jfalardeau@pyxis-tech.com>
2015-05-21 10:44:20 -0400

    Explored ISNI ASSIGNED requests (minimal and isnot requests)

--- test/integration/isni/test_isni_accept_environment_requests.py
+++ test/integration/isni/test_isni_accept_environment_requests.py
@@ -45,7 +45,7 @@ def request_has_assigned_an_isni(response):
 
 
 def request_was_ok(response):
-    assert_that(response.status_code, equal_to(requests.codes.not_acceptable), "response code")
+    assert_that(response.status_code, equal_to(requests.codes.ok), "response code")
 
 
 def request_was_not_acceptable(response):
@@ -90,42 +90,144 @@ def request_had_insufficient_information(response):
                 "insufficient information")
 
 
-@pytest.mark.wip
 def test_assign_isni_using_the_remote_accept_environment_with_a_well_formed_request(url, headers):
     payload = '''
-        <Request>
+        <Request xsi:noNamespaceSchemaLocation="ISNI%20request.xsd"
+                 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
             <requestID>
-                <dateTimeOfRequest>2011-05-20T09:09:35.5063705+02:00</dateTimeOfRequest>
-                <requestorTransactionId>5340</requestorTransactionId>
+                <dateTimeOfRequest>2001-12-17T09:30:47Z</dateTimeOfRequest>
+                <requestorTransactionId>My ID</requestorTransactionId>
             </requestID>
             <identityInformation>
                 <requestorIdentifierOfIdentity>
-                    <referenceURI>http://www.sos.nl</referenceURI>
-                    <identifier>13365</identifier>
+                    <referenceURI>www.rag.address.com</referenceURI>
+                    <identifier>1515161634</identifier>
+                </requestorIdentifierOfIdentity>
+                <otherIdentifierOfIdentity>
+                    <identifier>99667784</identifier>
+                    <type>IPD</type>
+                </otherIdentifierOfIdentity>
+                <identity>
+                    <personOrFiction>
+                        <personalName>
+                            <nameUse>public and private</nameUse>
+                            <surname>Nieuwname</surname>
+                            <forename>Pamela Williams</forename>
+                        </personalName>
+                        <gender>female</gender>
+                        <birthDate>1953-02-13</birthDate>
+                        <nationality>US</nationality>
+                        <resource>
+                            <creationClass>test</creationClass>
+                            <creationRole>aut</creationRole>
+                            <titleOfWork>
+                                <title>Men and demons</title>
+                                <imprint>
+                                    <publisher>HarperPerennial</publisher>
+                                </imprint>
+                                <identifier>
+                                    <identifierValue>9780060974961</identifierValue>
+                                    <identifierType>ISBN</identifierType>
+                                </identifier>
+                            </titleOfWork>
+                        </resource>
+                        <contributedTo>
+                            <titleOfCollectiveWorkOrWorkPerformed>
+                                Amercan History Journal
+                            </titleOfCollectiveWorkOrWorkPerformed>
+                            <identifier>
+                                <identifierType>ISSN</identifierType>
+                                <identifierValue>0010-0870</identifierValue>
+                            </identifier>
+                        </contributedTo>
+                        <personalNameVariant>
+                            <nameUse>private</nameUse>
+                            <surname>Williams</surname>
+                            <forename>Pamela Jane</forename>
+                        </personalNameVariant>
+                    </personOrFiction>
+                </identity>
+                <languageOfIdentity>eng</languageOfIdentity>
+                <countriesAssociated>
+                    <countryCode>US</countryCode>
+                </countriesAssociated>
+                <externalInformation>
+                    <information>wikipedia</information>
+                    <URI>http://en.wikipedia.org/wiki/Salem_witch_trials</URI>
+                </externalInformation>
+            </identityInformation>
+            <isRelated identityType="personOrFiction">
+                <relationType>co-author</relationType>
+                <noISNI>
+                    <PPN>082588929</PPN>
+                    <personalName>
+                        <nameUse>public</nameUse>
+                        <surname>Williams</surname>
+                        <forename>Selma R.</forename>
+                    </personalName>
+                </noISNI>
+            </isRelated>
+        </Request>
+    '''
+    response = requests.post(url, data=payload, headers=headers, verify=False)
+
+    request_was_ok(response)
+    request_has_assigned_an_isni(response)
+
+
+def test_assign_isni_using_the_remote_accept_environment_with_an_isnot_request(url, headers):
+    payload = '''
+        <Request xsi:noNamespaceSchemaLocation="ISNI%20request.xsd"
+                 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <requestID>
+                <dateTimeOfRequest>2012-11-09T09:30:47Z</dateTimeOfRequest>
+                <requestorTransactionId>multiple match1</requestorTransactionId>
+            </requestID>
+            <identityInformation>
+                <requestorIdentifierOfIdentity>
+                    <referenceURI>myURL</referenceURI>
+                    <identifier>11112222332324</identifier>
                 </requestorIdentifierOfIdentity>
                 <identity>
                     <personOrFiction>
                         <personalName>
                             <nameUse>public and private</nameUse>
-                            <surname>BAERTHEL</surname>
-                            <forename>JAN</forename>
+                            <surname>Adler</surname>
+                            <forename>Larry</forename>
                         </personalName>
+                        <birthDate>1914</birthDate>
+                        <deathDate>2001</deathDate>
                         <resource>
-                            <creationClass>
-                                <domain>literature </domain>
-                                <formOfPublication>book </formOfPublication>
-                                <pietjePuk>fi<p>lm</p></pietjePuk>
-                            </creationClass>
-                            <creationRole>aut</creationRole>
+                            <creationClass>jm</creationClass>
+                            <creationRole>prf</creationRole>
                             <titleOfWork>
-                                <title>Industrielles Bauen: Leitfaden f??MU-Gesch?sf??r</title>
+                                <title>St. Louis blues</title>
+                            </titleOfWork>
+                        </resource>
+                        <resource>
+                            <creationClass>jm</creationClass>
+                            <creationRole>prf</creationRole>
+                            <titleOfWork>
+                                <title>Beguine</title>
                             </titleOfWork>
                         </resource>
                     </personOrFiction>
                 </identity>
+                <note>should create a new record and not match with PPNs 37444949X, 36586272X and 083863184</note>
             </identityInformation>
+            <isNot identityType="personOrFiction">
+                <noISNI>
+                    <PPN>37444949X</PPN>
+                </noISNI>
+            </isNot>
+            <isNot identityType="personOrFiction">
+                <noISNI>
+                    <PPN>083863184</PPN>
+                </noISNI>
+            </isNot>
         </Request>
     '''
+
     response = requests.post(url, data=payload, headers=headers, verify=False)
 
     request_was_ok(response)
