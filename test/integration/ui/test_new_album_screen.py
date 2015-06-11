d360ea14afc4271b1422d04b2a6bd622d402fcdc
testinfected <vtence@gmail.com>
2015-06-11 16:32:59 -0400

    Disable keyboard shortcuts tests on Mac

--- test/integration/ui/test_new_album_screen.py
+++ test/integration/ui/test_new_album_screen.py
@@ -17,8 +17,9 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-from hamcrest import equal_to, has_property
+from hamcrest import has_property
 import pytest
+
 from cute.finders import WidgetIdentity
 from cute.probes import ValueMatcherProbe
 from test.drivers.new_album_screen_driver import NewAlbumScreenDriver
