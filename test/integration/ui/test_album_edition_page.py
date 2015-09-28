3bc208e60c26ee312befb7fd58dfa569622b7d90
Jonathan Falardeau <jonathan.falardeau@gmail.com>
2015-09-28 11:00:09 -0400

    Move some more tests to pytest.

---
+++ test/integration/ui/test_album_edition_page.py
@@ -0,0 +1,297 @@
+# -*- coding: utf-8 -*-
+import timeit
+
+from PyQt5.QtCore import QByteArray
+from hamcrest import has_entries, assert_that, less_than
+import pytest
+
+from cute.matchers import named
+from cute.probes import ValueMatcherProbe
+from cute.widgets import window
+from test.drivers import AlbumEditionPageDriver
+from test.integration.ui import show_widget
+from test.util import resources, builders as build
+from test.util.builders import make_album, make_anonymous_session, make_registered_session
+from tgit.metadata import Image
+from tgit.ui.album_edition_page import make_album_edition_page, AlbumEditionPage
+from tgit.util import fs
+
+
+@pytest.yield_fixture()
+def driver(qt, prober, automaton):
+    page_driver = AlbumEditionPageDriver(window(AlbumEditionPage, named("album_edition_page")), prober, automaton)
+    yield page_driver
+    page_driver.close()
+
+
+ignore = lambda *_, **__: None
+
+
+def show_track_page(album, session=make_anonymous_session(), select_picture=ignore, edit_performers=ignore, **handlers):
+    page = make_album_edition_page(album, session, edit_performers, select_picture, **handlers)
+    show_widget(page)
+    return page
+
+
+def test_displays_invalid_image_placeholder_when_album_has_corrupted_picture(driver):
+    _ = show_track_page(build.album(images=[build.image("image/jpeg", QByteArray(), Image.FRONT_COVER)]))
+    driver.shows_picture_placeholder()
+
+
+def test_displays_no_image_placeholder_when_album_has_no_cover(driver):
+    _ = show_track_page(build.album())
+    driver.shows_picture_placeholder()
+
+
+def test_displays_main_album_cover_when_existing(driver):
+    _ = show_track_page(build.album(
+        images=[build.image("image/jpeg", load_test_image("front-cover.jpg"), Image.FRONT_COVER)]))
+    driver.showsPicture()
+
+
+def test_displays_album_metadata(driver):
+    _ = show_track_page(build.album(
+        release_name="Album",
+        lead_performer="Artist",
+        isni="123456789",
+        guest_performers=[("Guitar", "Guitarist"), ("Piano", "Pianist")],
+        label_name="Label",
+        catalog_number="XXX123456789",
+        upc="123456789999",
+        recording_time="2008-09-15",
+        release_time="2009-01-01",
+        recording_studios="Studio A, Studio B",
+        producer="Artistic Producer",
+        mixer="Mixing Engineer",
+        comments="Comments\n...",
+        primary_style="Style"))
+
+    driver.showsReleaseName("Album")
+    driver.shows_compilation(False)
+    driver.shows_lead_performer("Artist")
+    driver.shows_isni("123456789", True)
+    driver.showsArea("")
+    driver.shows_guest_performers("Guitar: Guitarist; Piano: Pianist")
+    driver.showsLabelName("Label")
+    driver.showsCatalogNumber("XXX123456789")
+    driver.showsUpc("123456789999")
+    driver.shows_recording_time("2008-09-15")
+    driver.showsReleaseTime("2009-01-01")
+    driver.showsDigitalReleaseTime("2000-01-01")
+    driver.showsRecordingStudios("Studio A, Studio B")
+    driver.showsProducer("Artistic Producer")
+    driver.showsMixer("Mixing Engineer")
+    driver.showsComments("Comments\n...")
+    driver.shows_primary_style("Style")
+    driver.showsMediaType("")
+    # self.driver.showsReleaseType("")
+
+
+def test_indicates_whether_album_is_a_compilation(driver):
+    album = build.album(compilation=False)
+    _ = show_track_page(album)
+    driver.shows_compilation(False)
+
+    album.compilation = True
+    driver.shows_compilation(True)
+
+
+def test_disables_lead_performer_edition_when_album_is_a_compilation(driver):
+    _ = show_track_page(build.album(compilation=True, lead_performer="Album Artist"))
+    driver.shows_lead_performer("Various Artists", disabled=True)
+
+
+def test_enables_isni_lookup_when_album_is_no_longer_a_compilation(driver):
+    album = make_album(compilation=True, lead_performer="Album Artist")
+    _ = show_track_page(album, make_registered_session())
+    driver.enables_isni_lookup(False)
+
+    album.compilation = False
+    driver.enables_isni_lookup()
+
+
+def test_disables_isni_lookup_when_lead_performer_is_empty(driver):
+    _ = show_track_page(make_album(lead_performer=""), make_registered_session())
+    driver.enables_isni_lookup(False)
+
+
+def test_enables_isni_lookup_when_user_logs_in(driver):
+    session = make_anonymous_session()
+    _ = show_track_page(session=session, album=make_album(lead_performer="Album Artist"))
+    driver.enables_isni_lookup(False)
+
+    session.login_as("somebody@mgail.com", "api-key")
+    driver.enables_isni_lookup(True)
+
+
+def test_disables_isni_lookup_when_user_logs_out(driver):
+    session = make_registered_session()
+    _ = show_track_page(session=session, album=make_album(lead_performer="Album Artist"))
+    driver.enables_isni_lookup(True)
+
+    session.logout()
+    driver.enables_isni_lookup(False)
+
+
+def test_displays_tooltip_on_isni_lookup_when_anonymously_connected(driver):
+    session = make_anonymous_session()
+    _ = show_track_page(session=session, album=make_album(lead_performer="Album Artist"))
+    driver.isni_lookup_has_tooltip("Please sign-in to activate ISNI lookup")
+
+
+def test_removes_tooltip_on_isni_lookup_when_signed_in(driver):
+    session = make_registered_session()
+    _ = show_track_page(session=session, album=make_album(lead_performer="Album Artist"))
+    driver.isni_lookup_has_tooltip("")
+
+
+def test_disables_isni_lookup_when_lead_performer_is_blank(driver):
+    _ = show_track_page(make_album(lead_performer="     "))
+    driver.enables_isni_lookup(False)
+
+
+def test_disables_isni_assign_by_default(driver):
+    _ = show_track_page(make_album(lead_performer="     "))
+    driver.disables_isni_assign()
+
+
+def test_signals_when_picture_selected(driver):
+    album = build.album()
+    signal = ValueMatcherProbe("select picture", "front-cover.jpg")
+    _ = show_track_page(album, select_picture=lambda callback: callback("front-cover.jpg"),
+                        on_select_picture=signal.received)
+
+    driver.addPicture()
+    driver.check(signal)
+
+
+def test_updates_guest_performers_with_edition_dialog_return_value(driver):
+    _ = show_track_page(build.album(), edit_performers=lambda callback: callback(
+        [("instrument1", "performer1"), ("instrument2", "performer2")]))
+    driver.edit_performers()
+    driver.shows_guest_performers("instrument1: performer1; instrument2: performer2")
+
+
+def test_efficiently_displays_image_cover_when_it_does_not_change(driver):
+    album = build.album(images=[build.image("image/jpeg", load_test_image("big-image.jpg"), Image.FRONT_COVER)])
+    page = show_track_page(album)
+
+    time = timeit.timeit(lambda: page.display(album), number=50)
+    assert_that(time, less_than(1), "time to execute render 50 times")
+
+
+def test_signals_when_remove_picture_button_clicked(driver):
+    page = show_track_page(build.album())
+
+    remove_picture_signal = ValueMatcherProbe("remove picture")
+    page.remove_picture.connect(remove_picture_signal.received)
+
+    driver.removePicture()
+    driver.check(remove_picture_signal)
+
+
+def test_signals_when_lookup_isni_button_clicked(driver):
+    page = show_track_page(make_album(lead_performer="performer"), make_registered_session())
+
+    lookup_isni_signal = ValueMatcherProbe("lookup ISNI")
+    page.lookup_isni.connect(lookup_isni_signal.received)
+
+    driver.lookup_isni_of_lead_performer()
+    driver.check(lookup_isni_signal)
+
+
+def test_signals_when_clear_isni_button_clicked(driver):
+    page = show_track_page(build.album(isni="0000123456789"))
+
+    clear_isni_signal = ValueMatcherProbe("clear ISNI")
+    page.clear_isni.connect(clear_isni_signal.received)
+
+    driver.clear_isni()
+    driver.check(clear_isni_signal)
+
+
+def test_signals_when_assign_isni_button_clicked(driver):
+    page = show_track_page(build.album(lead_performer="performer"))
+
+    assign_isni_signal = ValueMatcherProbe("assign ISNI")
+    page.assign_isni.connect(assign_isni_signal.received)
+
+    driver.assign_isni_to_lead_performer()
+    driver.check(assign_isni_signal)
+
+
+def test_signals_when_album_metadata_edited(driver):
+    page = show_track_page(build.album())
+
+    metadata_changed_signal = ValueMatcherProbe("metadata changed")
+    page.metadata_changed.connect(metadata_changed_signal.received)
+
+    metadata_changed_signal.expect(has_entries(release_name="Title"))
+    driver.changeReleaseName("Title")
+    driver.check(metadata_changed_signal)
+
+    metadata_changed_signal.expect(has_entries(compilation=True))
+    driver.toggle_compilation()
+    driver.check(metadata_changed_signal)
+
+    metadata_changed_signal.expect(has_entries(compilation=False))
+    driver.toggle_compilation()
+    driver.check(metadata_changed_signal)
+
+    metadata_changed_signal.expect(has_entries(lead_performer="Artist"))
+    driver.changeLeadPerformer("Artist")
+    driver.check(metadata_changed_signal)
+
+    metadata_changed_signal.expect(has_entries(
+        guest_performers=[("Guitar", "Guitarist"), ("Guitar", "Bassist"), ("Piano", "Pianist")]))
+    driver.changeGuestPerformers("Guitar: Guitarist; Guitar: Bassist; Piano: Pianist")
+    driver.check(metadata_changed_signal)
+
+    metadata_changed_signal.expect(has_entries(label_name="Label"))
+    driver.changeLabelName("Label")
+    driver.check(metadata_changed_signal)
+
+    metadata_changed_signal.expect(has_entries(catalog_number="XXX12345678"))
+    driver.changeCatalogNumber("XXX12345678")
+    driver.check(metadata_changed_signal)
+
+    metadata_changed_signal.expect(has_entries(upc="123456789999"))
+    driver.changeUpc("123456789999")
+    driver.check(metadata_changed_signal)
+
+    metadata_changed_signal.expect(has_entries(comments="Comments\n...\n"))
+    driver.addComments("Comments")
+    driver.addComments("...")
+    driver.check(metadata_changed_signal)
+
+    metadata_changed_signal.expect(has_entries(release_time="2009-01-01"))
+    driver.changeReleaseTime(2009, 1, 1)
+    driver.check(metadata_changed_signal)
+
+    metadata_changed_signal.expect(has_entries(recording_time="2008-09-15"))
+    driver.change_recording_time(2008, 9, 15)
+    driver.check(metadata_changed_signal)
+
+    metadata_changed_signal.expect(has_entries(recording_studios="Studios"))
+    driver.changeRecordingStudios("Studios")
+    driver.check(metadata_changed_signal)
+
+    metadata_changed_signal.expect(has_entries(producer="Producer"))
+    driver.changeProducer("Producer")
+    driver.check(metadata_changed_signal)
+
+    metadata_changed_signal.expect(has_entries(mixer="Mixer"))
+    driver.changeMixer("Mixer")
+    driver.check(metadata_changed_signal)
+
+    metadata_changed_signal.expect(has_entries(primary_style="Jazz"))
+    driver.select_primary_style("Jazz")
+    driver.check(metadata_changed_signal)
+
+    metadata_changed_signal.expect(has_entries(primary_style="Custom"))
+    driver.changePrimaryStyle("Custom")
+    driver.check(metadata_changed_signal)
+
+
+def load_test_image(name):
+    return fs.read(resources.path(name))
---
+++ test/local_storage/test_csv_format.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+
+from io import StringIO
+import csv
+
+from hamcrest import assert_that, contains, has_item
+
+from hamcrest.core.core.isequal import equal_to
+import pytest
+
+from test.util import builders as build
+from tgit.local_storage.csv_format import CsvFormat, toBoolean
+
+
+@pytest.fixture
+def formatter():
+    return CsvFormat()
+
+
+@pytest.fixture
+def out():
+    return StringIO()
+
+
+def test_includes_header_row(formatter, out):
+    album = build.album()
+    formatter.write(album, out)
+
+    rows = read_csv(out)
+    headers = next(rows)
+    assert_that(headers, contains("Release Name",
+                                  "Compilation",
+                                  "Lead Performer",
+                                  "Lead Performer ISNI",
+                                  "Guest Performers",
+                                  "Label Name",
+                                  "Catalog Number",
+                                  "UPC/EAN",
+                                  "Comments",
+                                  "Release Date",
+                                  "Recording Date",
+                                  "Recording Studios",
+                                  "Producer",
+                                  "Mixer",
+                                  "Primary Style",
+                                  "Track Title",
+                                  "Version Information",
+                                  "Track Number",
+                                  "Total Tracks",
+                                  "Featured Guest",
+                                  "Lyrics",
+                                  "Language",
+                                  "Publisher",
+                                  "Lyricist",
+                                  "Composer",
+                                  "ISRC",
+                                  "Tags"), "header")
+
+
+def test_writes_track_metadata_in_columns(formatter, out):
+    album = build.album(
+        release_name="Release Name",
+        lead_performer="Lead Performer",
+        isni="0000123456789",
+        guest_performers=[("Instrument1", "Performer1"), ("Instrument2", "Performer2")],
+        label_name="Label Name",
+        catalog_number="Catalog Number",
+        upc="Barcode",
+        comments="Comments\n...\n...",
+        release_time="2014",
+        recording_time="2013",
+        recording_studios="Studios",
+        producer="Artistic Producer",
+        mixer="Mixing Engineer",
+        primary_style="Genre")
+
+    track = build.track(
+        track_title="Track Title",
+        versionInfo="Version Info",
+        featuredGuest="Featuring",
+        lyrics="Lyrics\n...\...\n...",
+        language="eng",
+        publisher="Publisher",
+        lyricist="Lyricist",
+        composer="Composer",
+        isrc="ISRC",
+        labels="Tag1 Tag2 Tag3")
+
+    album.addTrack(track)
+    track.track_number = 3
+    track.total_tracks = 9
+
+    formatter.write(album, out)
+
+    rows = read_csv(out)
+    _ = next(rows)
+    data = next(rows)
+    assert_that(data, contains("Release Name",
+                               "False",
+                               "Lead Performer",
+                               "0000123456789",
+                               "Instrument1: Performer1; Instrument2: Performer2",
+                               "Label Name",
+                               "Catalog Number",
+                               "Barcode",
+                               "Comments\r...\r...",
+                               "2014",
+                               "2013",
+                               "Studios",
+                               "Artistic Producer",
+                               "Mixing Engineer",
+                               "Genre",
+                               "Track Title",
+                               "Version Info",
+                               "3",
+                               "9",
+                               "Featuring",
+                               "Lyrics\r...\...\r...",
+                               "eng",
+                               "Publisher",
+                               "Lyricist",
+                               "Composer",
+                               "ISRC",
+                               "Tag1 Tag2 Tag3"), "row")
+
+
+def test_converts_booleans_to_text():
+    assert_that(toBoolean(None), equal_to("False"), "boolean(None)")
+    assert_that(toBoolean(False), equal_to("False"), "boolean(False)")
+    assert_that(toBoolean(True), equal_to("True"), "boolean(True)")
+
+
+def test_writes_one_record_per_track_in_album(formatter, out):
+    album = build.album(tracks=[build.track(track_title="Song 1"),
+                                build.track(track_title="Song 2"),
+                                build.track(track_title="Song 3")])
+
+    formatter.write(album, out)
+
+    rows = read_csv(out)
+    _ = next(rows)
+    assert_that(next(rows), has_item("Song 1"), "first row")
+    assert_that(next(rows), has_item("Song 2"), "second row")
+    assert_that(next(rows), has_item("Song 3"), "third row")
+
+
+def test_make_line_breaks_excel_friendly_by_converting_line_feeds_to_carriage_returns(formatter, out):
+    album = build.album(comments="Comments\nspanning\nseveral lines", tracks=[build.track()])
+
+    formatter.write(album, out)
+
+    csv_file = read_csv(out)
+    _ = next(csv_file)
+    assert_that(next(csv_file), has_item("Comments\rspanning\rseveral lines"), "row with line feeds")
+
+
+def read_csv(output_string):
+    output_string.seek(0)
+    return csv.reader(output_string)
