3d241bc52c6da4a085d2bd38f4bd653b9bbf3095
Jonathan Falardeau <jonathan.falardeau@gmail.com>
2015-11-04 17:30:02 -0500

    Add the missing SOPROQ required fields.

--- tgit/export/soproq_format.py
+++ tgit/export/soproq_format.py
@@ -17,21 +17,29 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 from datetime import date
+from tgit.countries import ISO3166_2_A2_TO_ISO3166_2_A3
 
 
 def _to_duration(seconds):
     return seconds and "00:%02d:%02d" % divmod(round(seconds), 60) or ""
 
 
+def _to_iso_3_letters_code(region):
+    return ISO3166_2_A2_TO_ISO3166_2_A3[region[0]] if region else ""
+
+
 def _write_line(album, track, line_number, sheet):
     album_lead_performer = album.lead_performer if not album.compilation else "Artistes Variés"
     track_lead_performer = track.lead_performer if track.lead_performer else album_lead_performer
+    lead_performer_region = _to_iso_3_letters_code(album.lead_performer_region)
+    recording_studio_region = _to_iso_3_letters_code(album.recording_studio_region)
+    initial_producer_region = _to_iso_3_letters_code(album.initial_producer_region)
     recording_time = album.recording_time.split("-")[0] if album.recording_time else ""
     compilation = "O" if album.compilation else "N"
 
     sheet["A"+str(line_number)] = album.release_name    # TITRE ALBUM
     sheet["B"+str(line_number)] = album_lead_performer  # INTERPRÈTE ALBUM
-    sheet["C"+str(line_number)] = ""                    # NATIONALITÉ INTERPRÈTE ALBUM
+    sheet["C"+str(line_number)] = lead_performer_region  # NATIONALITÉ INTERPRÈTE ALBUM
     sheet["D"+str(line_number)] = album.label_name      # MAISON DE DISQUES
     sheet["E"+str(line_number)] = album.label_name      # ÉTIQUETTE
     sheet["F"+str(line_number)] = ""                    # DISTRIBUTEUR
@@ -43,13 +51,13 @@ def _write_line(album, track, line_number, sheet):
     sheet["L"+str(line_number)] = str(track.track_number)  # NO. PLAGE
     sheet["M"+str(line_number)] = track.track_title     # * TITRE ENREGISTREMENT SONORE
     sheet["N"+str(line_number)] = track_lead_performer  # * INTERPRÈTE ENREGISTREMENT SONORE
-    sheet["O"+str(line_number)] = ""                    # * NATIONALITÉ INTERPRÈTE
+    sheet["O"+str(line_number)] = lead_performer_region  # * NATIONALITÉ INTERPRÈTE
     sheet["P"+str(line_number)] = track.isrc            # * ISRC
     sheet["Q"+str(line_number)] = _to_duration(track.duration)  # DURÉE
-    sheet["R"+str(line_number)] = ""                    # * PAYS DE FIXATION
+    sheet["R"+str(line_number)] = recording_studio_region  # * PAYS DE FIXATION
     sheet["S"+str(line_number)] = recording_time        # * ANNÉE DE FIXATION
-    sheet["T"+str(line_number)] = album.artistic_producer  # * PRODUCTEUR INITIAL
-    sheet["U"+str(line_number)] = ""                    # * NATIONALITÉ PRODUCTEUR
+    sheet["T"+str(line_number)] = album.initial_producer  # * PRODUCTEUR INITIAL
+    sheet["U"+str(line_number)] = initial_producer_region  # * NATIONALITÉ PRODUCTEUR
     sheet["V"+str(line_number)] = ""                    # * TYPE DE DROIT
     sheet["W"+str(line_number)] = ""                    # * POURCENTAGE
     sheet["X"+str(line_number)] = ""                    # * TYPE TERRITOIRE
