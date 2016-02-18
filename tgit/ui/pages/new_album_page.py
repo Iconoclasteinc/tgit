dea13e29a845828ec3e20ccbc1c40fa0efc4017b
testinfected <vtence@gmail.com>
2016-02-18 13:31:14 -0500

    Fix previous merge where I accidentatly picked up the old versions of the files

--- tgit/ui/pages/new_album_page.py
+++ tgit/ui/pages/new_album_page.py
@@ -17,7 +17,9 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-from PyQt5.QtWidgets import QFrame
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QKeySequence
+from PyQt5.QtWidgets import QFrame, QDialogButtonBox
 
 from tgit.album import Album
 from tgit.ui import locations
@@ -37,6 +39,8 @@ def make_new_project_page(select_location, select_track, check_project_exists, c
 
 
 class NewProjectPage(QFrame, UIFile):
+    _project_type = Album.Type.FLAC
+
     def __init__(self, select_location, select_track, check_project_exists, confirm_overwrite):
         super().__init__()
         self._confirm_overwrite = confirm_overwrite
@@ -45,45 +49,54 @@ class NewProjectPage(QFrame, UIFile):
 
     def _setup_ui(self, select_destination, select_track_location):
         self._load(":/ui/new_album_page.ui")
-        self.addAction(self._cancel_creation_action)
-        self.addAction(self._create_project_action)
+        self._create_button().setText(self.tr("Create"))
+        self._create_button().setShortcut(QKeySequence(Qt.Key_Return))
+        self._cancel_button().setShortcut(QKeySequence(Qt.Key_Escape))
         self._location.textChanged.connect(self._toggle_create_button)
         self._name.textChanged.connect(self._toggle_create_button)
-        self._browse_location_button.clicked.connect(
-            lambda: select_destination(self._location.setText))
-        self._browse_track_location_button.clicked.connect(
-            lambda: select_track_location(self._type(), self._track_location.setText))
+        self._browse_location_button.clicked.connect(lambda: select_destination(self._location.setText))
+        self._select_reference_track_button.clicked.connect(
+                lambda: select_track_location(self.project_type, self._reference_track.setText))
+
+    def _create_button(self):
+        return self._action_buttons.button(QDialogButtonBox.Ok)
+
+    def _cancel_button(self):
+        return self._action_buttons.button(QDialogButtonBox.Cancel)
 
     def on_create_project(self, on_create_project):
         def create_project():
-            on_create_project(self._type(), self._name.text(), self._location.text(),
-                              self._track_location.text())
+            on_create_project(self.project_type, self._name.text(), self._location.text(), self._reference_track.text())
 
-        def check_project_exists():
+        def confirm_project_creation():
             if self._project_exists(self._name.text(), self._location.text()):
                 self._confirm_overwrite(on_accept=create_project)
             else:
                 create_project()
 
-        self._create_project_action.triggered.connect(check_project_exists)
+        self._action_buttons.accepted.connect(confirm_project_creation)
 
     def on_cancel_creation(self, on_cancel_creation):
-        self._cancel_creation_action.triggered.connect(on_cancel_creation)
+        self._action_buttons.rejected.connect(on_cancel_creation)
 
     def _toggle_create_button(self):
-        self._create_button.setEnabled(self._can_create())
-        self._create_project_action.setEnabled(self._can_create())
+        self._create_button().setEnabled(self._can_create())
 
     def _can_create(self):
         return self._location.text() != "" and self._name.text() != ""
 
-    def _type(self):
-        return Album.Type.FLAC if self._flac_button.isChecked() else Album.Type.MP3
+    @property
+    def project_type(self):
+        return self._project_type
+
+    @project_type.setter
+    def project_type(self, project_type):
+        self._project_type = project_type
+        self._title.setText(self.tr("New {} Project".format(project_type.upper())))
 
     def showEvent(self, event):
-        self._mp3_button.setChecked(True)
         self._name.setText(self.tr("untitled"))
         self._name.setFocus()
         self._name.selectAll()
         self._location.setText(locations.Documents)
-        self._track_location.setText("")
+        self._reference_track.setText("")
