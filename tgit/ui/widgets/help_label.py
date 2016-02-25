b02c9af0a82f2bc64df4b06ae13010c9dadf168e
testinfected <vtence@gmail.com>
2016-02-25 11:55:55 -0500

    Use a custom widget to show tooltip instantly for help labels and change to a double resolution question mark icon

---
+++ tgit/ui/widgets/help_label.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+# TGiT, Music Tagger for Professionals
+# Copyright (C) 2013 Iconoclaste Musique Inc.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+from PyQt5.QtCore import QEvent
+from PyQt5.QtWidgets import QLabel, QToolTip
+
+
+class HelpLabel(QLabel):
+    def __init__(self, *__args):
+        super().__init__(*__args)
+        self.setMouseTracking(True)
+        self.setStyleSheet("""
+            QToolTip {
+                padding: 2px;
+                background-color: palette(window);
+            }
+        """)
+
+    def enterEvent(self, event):
+        QToolTip.showText(event.globalPos(), self.toolTip(), self)
+
+    def event(self, event):
+        if event.type() == QEvent.ToolTip:
+            event.ignore()
+            return True
+        else:
+            return super().event(event)
